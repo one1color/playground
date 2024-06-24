9c9cdd08222c750607ecc66c040200ce13d2ea04
one1color <koki.111.sui@gmail.com>
2024-06-24 19:27:43 +0200

    wip

---
+++ forEachAndMap.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: [⇐ Previous: 05 - Challenge - Closure Syntax](@previous)
+//: ## Episode 06: forEach & map
+import Foundation
+
+// --------------------------------------
+var prices = [1.50, 10.00, 4.99, 2.30, 8.19]
+// --------------------------------------
+
+//: `forEach` - Loops over a collection and performs an operation
+for price in prices {
+    print(price)
+}
+
+prices.forEach {
+    print($0)
+}
+
+
+//: `map` - Loops over an array, executes closure code, returns a new array
+var arrayForSalePrices: [Double] = []
+for price in prices {
+    arrayForSalePrices.append(price * 0.9)
+}
+
+let salePrices = prices.map { $0 * 0.9 }
+
+let salePrices2 = prices.map { price -> Double in
+    price * 0.9
+}
+
+let priceLabels = salePrices.map { String(format: "%.2f", $0) }
+
+//: [⇒ Next: 07 - compactMap & flatMap](@next)
